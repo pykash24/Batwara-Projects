0f5818e7eefac75341101c061460981c50ce27b1
anupama-qd <anupama.borkar@qd-corp.com>
2023-04-16 16:25:29 +0530

    bottom sheet development AB

--- split_contro/src/screens/profile/EditProfile.jsx
+++ split_contro/src/screens/profile/EditProfile.jsx
@@ -1,48 +1,55 @@
-import { Button } from 'react-native-paper';
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
+import React from 'react'
+import {Gesture,GestureDetector} from 'react-native-gesture-handler'
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
+import { Colors } from '../../constants/Colors'
 
-import Animated, {
-    withSpring,
-    useAnimatedStyle,
-    useSharedValue,
-  } from 'react-native-reanimated';
-  
-  function EditProfile() {
-    const offset = useSharedValue(0);
-  
-    const defaultSpringStyles = useAnimatedStyle(() => {
-      return {
-        transform: [{ translateX: withSpring(offset.value * 255) }],
-      };
-    });
-  
-    const customSpringStyles = useAnimatedStyle(() => {
-      return {
-        transform: [
-          {
-            translateX: withSpring(offset.value * 255, {
-              damping: 20,
-              stiffness: 90,
-            }),
-          },
-        ],
-      };
-    });
-  
-    return (
-      <>
-        <Animated.View style={[styles.box, defaultSpringStyles]} />
-        <Animated.View style={[styles.box, customSpringStyles]} />
-        <Button style={[styles.box]}  onPress={() => (offset.value = Math.random())} title="Move" />
-      </>
-    );
-  }
-  export default EditProfile
-  
-const styles = StyleSheet.create({
-    box: {
-      width:200,
-      height:89,
-      background: 'black',
+const {height:SCREEN_HEIGHT}=Dimensions.get('window')
+const EditProfile = () => {
+  const translateY=useSharedValue(0)
+
+  const context=useSharedValue({y:0})
+  const gesture =Gesture.Pan()
+  .onStart(()=>{
+    context.value={y:translateY.value}
+  })
+  .onUpdate((event)=>{
+    console.log('gg',event);
+    translateY.value=event.translationY + context.value.y;
+  });
+
+  const rBottomStyle=useAnimatedStyle(()=>{
+    return{
+      transform:[{translateY:translateY.value}]
     }
-})  
+  })
+  return (
+   <GestureDetector gesture={gesture}>
+    <Animated.View style={[styles.bottomSheetContainer,rBottomStyle]}>
+      <View style={styles.line}></View>
+    </Animated.View>
+   </GestureDetector>
+  )
+}
+
+export default EditProfile
+
+const styles = StyleSheet.create({
+  bottomSheetContainer:{
+    height:SCREEN_HEIGHT,
+    width:'100%',
+    backgroundColor:Colors.white,
+    position:'absolute',
+    top:SCREEN_HEIGHT/2.5,
+    borderRadius:25
+  },
+  line:{
+    width:75,
+    height:4,
+    backgroundColor:Colors.gray,
+    alignSelf:'center',
+    marginVertical:15,
+    borderRadius:2
+
+  },
+})
